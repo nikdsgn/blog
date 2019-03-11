7d4b415304e095f8bf88eb078ad91fe62ccc32af
N R <nr@Ns-MacBook-Pro.local>
2019-03-11 19:32:11 +0100

    Add gitignore, procfile, update ports

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-nik:Testat123@cluster0-xrkil.mongodb.net/blogDB", {useNewUrlParser: true});
 
 
 // let posts = [];
@@ -100,8 +100,12 @@ app.get("/contact", function(req, res){
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started Succesfully!");
 });
